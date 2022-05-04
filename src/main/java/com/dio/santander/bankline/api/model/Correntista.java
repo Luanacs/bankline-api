4dc343349cc228be4f9fffefe95169026369dba2
Luana Chaves <luanacsiqueira@hotmail.com.br>
2022-05-04 08:49:18 -0300

    Criando classes controller

--- src/main/java/com/dio/santander/bankline/api/model/Correntista.java
+++ src/main/java/com/dio/santander/bankline/api/model/Correntista.java
@@ -1,17 +1,34 @@
 package com.dio.santander.bankline.api.model;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="tab_correntista")
 public class Correntista {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	
+	@Column(length =20)
 	private String cpf;
+	
+	@Column(length=60)
 	private String nome;
 	
+	@Embedded //as informações que estiverem nessa classe estarão na mesma tabela que a tabela principal
 	private Conta conta;
 	
 	public Conta getConta() {
 		return conta;
 	}
-	public void setConta() {
+	public void setConta(Conta conta) {
 		this.conta=conta;
 	}
 	
